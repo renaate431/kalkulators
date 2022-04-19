a6b7e3a290c17b12ca8b14f899a1ac644677c32b
renaate431 <r.goldmane@gmail.com>
2022-04-19 09:29:57 +0300

    OTRS.1
    strādā +,-,= un C

--- kalkulators.py
+++ kalkulators.py
@@ -1,3 +1,4 @@
+import re
 from tkinter import*
 mansLogs=Tk() #saiisinājums tkinker
 mansLogs.title("+-*/=calc")# iedod nosaukumu
@@ -5,8 +6,10 @@ mansLogs.title("+-*/=calc")# iedod nosaukumu
 #poga=Button(mansLogs, text="hey!", bg="green", fg="white") #bg -background fg=priekšplāna krāsa
 #pack- parādīt
 #poga.pack()
-e=Entry(mansLogs,width=15, font=("Arial Black",20))
+e=Entry(mansLogs,width=15, font=("Arial Black",20), bg=("black"), fg=("white"))
 e.grid(row=0,column=0,columnspan=4)
+
+
 def btnClick(number):
     current=e.get()#nolasa esošo skaitli
     e.delete(0,END)#nodzēš
@@ -14,6 +17,8 @@ def btnClick(number):
     e.insert(0,newNumber)# ievieto displejā
     return 0
 
+
+#-------------------------------------------------------------------------
 def btnCommand(command):
     global num1 #jāiegaumē skaitlis un darbība
     global mathOp #matemātikas operators
@@ -21,6 +26,37 @@ def btnCommand(command):
     num1= int(e.get())
     e.delete(0,END)
     return 0
+
+ #----------------------------------------------------------------------   
+
+def equal(command):
+    global num2
+    num2=(int(e.get()))
+    result=0
+    if mathOp=="-":
+        result=num1-num2
+    elif mathOp=="+":
+        result= num1+num2
+    elif mathOp=="*":
+        result= num1*num2
+    elif mathOp=="/":
+        result=num1/num2
+    else:
+        result=0
+    e.delete(0,END)
+    e.insert(0,str(result))
+    return 0
+
+#-----------------------------------------------------------------------------------
+def clear(command):
+    e.delete(0,END)
+    num1=0
+    mathOp=""
+    return 0
+
+#------------------------------------------------------------------------------------
+
+
 
 btn0=Button(mansLogs, text="0 ", padx="40", pady="20", command=lambda:btnClick(0),bg="black", fg="hot pink")# funkcija 
 btn1=Button(mansLogs, text="1 ", padx="40", pady="20", command=lambda:btnClick(1),bg="black", fg="hot pink")
@@ -36,8 +72,8 @@ btn10=Button(mansLogs,text="/ ", padx="40", pady="20", command=lambda:btnCommand
 btn11=Button(mansLogs,text="* ", padx="40", pady="20", command=lambda:btnCommand("*"),bg="black", fg="hot pink")
 btn12=Button(mansLogs,text="+ ", padx="40", pady="20", command=lambda:btnCommand("+"),bg="black", fg="hot pink")
 btn13=Button(mansLogs,text="- ", padx="40", pady="20", command=lambda:btnCommand("-"),bg="black", fg="hot pink")
-btn14=Button(mansLogs,text="= ", padx="40", pady="20",bg="black", fg="hot pink")
-btn15=Button(mansLogs,text="<-",padx="40", pady="20",bg="black", fg="hot pink")
+btn14=Button(mansLogs,text="= ", padx="40", pady="20",bg="black", fg="hot pink", command=lambda:equal("="))
+btn15=Button(mansLogs,text="C",padx="40", pady="20",bg="black", fg="hot pink", command=lambda:clear("C"))
 
 
 
